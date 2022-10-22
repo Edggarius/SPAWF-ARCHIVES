13eab6cd0dc7576d17d1b424ca0d4420c6435905
Giovani88 <l20120091@morelia.tecnm.mx>
2022-10-21 20:03:56 -0500

    Creando Historia.html

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home/home.html',{})
 # Create your views here.
+def historia(request):
+    return render(request, 'home/historia.html',{})
