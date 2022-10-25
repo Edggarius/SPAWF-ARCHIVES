86dd6cf130b86a522fe25f02faae736ca8646b17
Ernesto García Bernal <ern.garbe@gmail.com>
2022-10-25 15:53:31 -0500

    Agregamos curso y cursos

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('historia/', views.historia, name='historia'),
     path('servicios/', views.servicios, name='servicios'),
+    path('cursos/', views.cursos, name='cursos'),
+    path('curso/', views.curso, name='curso'),
+    
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -8,3 +8,9 @@ def historia(request):
 
 def servicios(request):
     return render(request, 'home/servicios.html',{})
+
+def cursos(request):
+    return render(request, 'home/cursos.html',{})
+
+def curso(request):
+    return render(request, 'home/curso.html',{})
