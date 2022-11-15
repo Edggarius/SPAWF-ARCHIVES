516b7b591cf1289c52ec2e41cdf358e21d3efae5
Giovani88 <l20120091@morelia.tecnm.mx>
2022-11-14 23:43:27 -0600

    aasd

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 DJANGO_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ DJANGO_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
 
 PROJECT_APPS=[
     'apps.autentificacion',
